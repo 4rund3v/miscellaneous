201db36ca6683bebf6344b974e97da923248ea19
arun dev <4rund3v@gmail.com>
2020-11-29 11:14:54 +0530

    added default nginx conf to be replaced after installation.

--- nginx_setup/setup_nginx_service.sh
+++ nginx_setup/setup_nginx_service.sh
@@ -14,10 +14,10 @@ sudo nginx -t
 sudo mkdir -p /var/cache/nginx/{client_temp,fastcgi_temp,proxy_temp,scgi_temp,uwsgi_temp}
 sudo chmod 700 /var/cache/nginx/*
 sudo chown nginx:root /var/cache/nginx/*
-
+# copying the default nginx conf prepared
+sudo cp -v ./nginx.conf /etc/nginx/nginx.conf
 # Re-check syntax and potential errors. 
 sudo nginx -t
-
 echo "Creating the nginx service."
 sudo cp -v ./nginx.service /etc/systemd/system/nginx.service
 echo "Enabling the nginx as a service."
@@ -27,8 +27,9 @@ echo "Creating the default directories."
 sudo mkdir -p /etc/nginx/{conf.d,snippets,sites-available,sites-enabled}
 echo "Creating th nginx log files."
 sudo chmod 640 /var/log/nginx/*
+sudo touch /var/log/nginx/access.log /var/log/nginx/error.log
 echo "Changing the access and error log."
 sudo chown nginx:root /var/log/nginx/access.log /var/log/nginx/error.log
 echo "Copying the log rotate files"
 sudo cp ./nginx.logrotate  /etc/logrotate.d/nginx
-
+sudo nginx -s reload
